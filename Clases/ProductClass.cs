ad642970fe9a47092a08b9c4ecd41e8f339759b1
Sizm12 <s.zeledon.cbe@gmail.com>
2023-10-23 00:50:44 -0600

    Ejemplo POO

---
+++ Clases/ProductClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Practica.Clases
+{
+    public class ProductClass
+    {
+        private string Product_Id { get; set; }
+        private string ProductName { get; set; }
+        private string ProductCategory { get; set; }
+        private float ProductPrice { get; set; }
+        private bool ProductState { get; set; } = true;
+
+        private void GenerateID()
+        {
+            var newGuid = Guid.NewGuid().ToString("N").Substring(0, 6);
+            Product_Id = newGuid;
+        }
+
+        public void AddProduct()
+        {
+            this.GenerateID();
+            Console.WriteLine("Ingrese los Datos Solicitados");
+            Console.WriteLine("Ingrese el Nombre del Producto");
+            ProductName = Console.ReadLine();
+            Console.WriteLine("Ingrese el Categoria del Producto");
+            ProductCategory = Console.ReadLine();
+            Console.WriteLine("Ingrese el Precio del Producto");
+            ProductPrice = float.Parse(Console.ReadLine());
+        }
+
+        public void GetProduct()
+        {
+            Console.WriteLine("Los Datos del Producto son:");
+            Console.WriteLine("El Id del Producto es: " + Product_Id);
+            Console.WriteLine("El Nombre del Producto es: " + ProductName);
+            Console.WriteLine("La Categoria del Producto es: " + ProductCategory);
+            Console.WriteLine("El Precio del Producto es: " + ProductPrice);
+            Console.WriteLine("El Estado del Producto es: " + ProductState);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POO_Practica.Clases;
+
+namespace POO_Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductClass product = new ProductClass();
+            int Resultado = 0;
+            do
+            {
+                Console.WriteLine("Catalogo de Productos");
+                Console.WriteLine("");
+                Console.WriteLine("1.- Agregar un Producto");
+                Console.WriteLine("2.- Visualizar un Producto");
+                Console.WriteLine("3.- Salir");
+                Console.WriteLine("");
+                Console.Write("Opcion: ");
+                Resultado = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                switch (Resultado)
+                {
+                    case 1:
+                        product.AddProduct();
+                        break;
+                    case 2:
+                        product.GetProduct();
+                        break;
+                    case 3:
+                        Console.WriteLine("Fin de la operacion");
+                        Console.Read();
+                        break;
+                    default:
+                        Console.WriteLine("ERROR, Opcion Invalida...");
+                        Console.ReadLine();
+                        Console.WriteLine("");
+                        Console.Clear();
+                        break;
+                }
+
+            } while (Resultado != 3);
+
+
+        }
+    }
+}
